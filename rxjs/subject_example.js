9a62a3923267b64143377742fa46697e1fb61816
mithunkumarc <mithunchannappa@gmail.com>
2019-11-03 08:14:15 +0530

    Update subject_example.js

--- rxjs/subject_example.js
+++ rxjs/subject_example.js
@@ -25,3 +25,40 @@ subject.next(2);
   observer A : 2
   observer B : 2
  */
+
+
+/* another example */
+
+import { Subject, from } from 'rxjs';
+
+/*
+  subject as an observable , it can also be a observer and subscribe to another observable
+*/ 
+
+const subject = new Subject();
+
+subject.subscribe({
+  next: (v) => console.log(`observerA: ${v}`)
+});
+
+subject.subscribe({
+  next: (v) => console.log(`observerB: ${v}`)
+});
+
+
+const observable = from([1,2,3]);
+
+
+// any one who subscribe to subject also subscribed to 
+// observable
+observable.subscribe(subject);
+
+/* 
+  output : 
+  observerA: 1
+  observerB: 1
+  observerA: 2
+  observerB: 2
+  observerA: 3
+  observerB: 3
+*/
